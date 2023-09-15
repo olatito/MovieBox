7014f3ed038c3676036edb6dae073483212d2c55
TITO's MACBOOK <user@TITOs-MacBook-Pro.local>
2023-09-16 00:09:48 +0100

    main

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header(){
+    return(
+        <div className="imageLogo">
+            <img src="assets/img/Logo.png" />
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/views/Dashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../components/Header/Header"
+function Dashboard(){
+    return(
+        // <Header />
+        <div>
+
+        </div>
+    )
+}
+
+export default Dashboard;
